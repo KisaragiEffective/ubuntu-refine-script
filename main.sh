f9ac43cd09a402636eb42c1805e3873632e11115
KisaragiEffective <kisaragi.effective@gmail.com>
2022-05-11 10:12:54 +0900

    feat: install termurin-8-jdk

--- main.sh
+++ main.sh
@@ -1,1 +1,18 @@
+function install_all() {
+  sudo apt-get install temurin-8-jdk
+}
+
+function prepare_all() {
+  prepare_termurin-8-jdk
+}
+
+function prepare_termurin-8-jdk() {
+  sudo apt-get install -y wget apt-transport-https
+  sudo wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /usr/share/keyrings/adoptium.asc
+  sudo echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+}
+
+prepare_all
+install_all
+
 echo "done"
